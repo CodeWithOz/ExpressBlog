02a8eef18d392af94e6e79c08ae286284eded2ce
CodeWithOz <ucheozoemena@gmail.com>
2019-03-09 19:46:08 +0100

    Configure app settings

---
+++ apiKeys_TEMPLATE.js
@@ -0,0 +1,4 @@
+const client_secret = 'CLIENT_SECRET_KEY';
+const session_secret = 'SESSION_SECRET_KEY';
+
+module.exports = { client_secret, session_secret };
--- app.js
+++ app.js
@@ -1,25 +1,73 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const okta = require('@okta/okta-sdk-nodejs');
+const session = require('express-session');
+const ExpressOIDC = require('@okta/oidc-middleware').ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { client_secret, session_secret } = require('./apiKeys');
 
-var app = express();
+const blogRouter = require('./routes/blog');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const client = new okta.Client({
+  orgUrl: 'https://dev-431001.okta.com',
+  token: '00Nui1O_QJeUqLWGXHcpiDJkVdyV8c3hGSqTOamAjk'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+const oidc = new ExpressOIDC({
+  issuer: 'https://dev-431001.okta.com/oauth2/default',
+  client_id: '0oacf8iavPMGwTWS1356',
+  client_secret,
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: 'openid profile',
+  routes: {
+    login: {
+      path: '/users/login'
+    },
+    callback: {
+      path: '/users/callback',
+      defaultRedirect: '/dashboard'
+    }
+  }
+});
+
+app.use(
+  session({
+    secret: session_secret,
+    resave: true,
+    saveUninitialized: false
+  })
+);
+
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  client.getUser(req.userinfo.sub).then(user => {
+    req.user = user;
+    res.locals.user = user;
+    next();
+  });
+});
+
+// Routes
+app.use('/', blogRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
